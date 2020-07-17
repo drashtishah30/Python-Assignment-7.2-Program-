31e5a48a721068e54baea64729c11051f44756e6
DRASHTI SHAH <drashti0230@gmail.com>
2020-07-17 11:12:41 +0530

    Delete assignment7.py

--- assignment7.py
+++
@@ -1,9 +0,0 @@
-fname = input("Enter file name: ")
-fh = open(fname)
-count = 0
-total = 0
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:") : continue
-    t=line.find("0")
-    number= float(line[t:])
-    print(t)
